a76bcf2184494cb112be111ddced3b71274d57c2
Harsh Kapadia <contact@harshkapadia.me>
2023-11-19 16:41:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Badger 2040
+
+My experimentation with the [Badger 2040](https://shop.pimoroni.com/products/badger-2040) hardware that I got at [GitHub Universe](https://githubuniverse.com) 2023.
+
+Context and details: https://github.com/badger2040/home
+
+## Examples
+
+### Marquee Name
+
+https://github.com/HarshKapadia2/badger-2040/assets/50140864/aa9a8f42-4cdd-436f-9213-995064bbd292
+
+-   Loops text from left to right like [the HTML Marquee tag](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/marquee).
+-   Buttons
+    -   `A`: Start looping
+    -   `C`: Stop looping
+    -   `B`: Display instructions
+-   Files: `examples/mname.py` and `examples/icon-mname.jpg`
+
+## Resources
+
+-   [Get started (with some context)](https://github.com/badger2040/home)
+-   [Getting Started with Badger 2040](https://learn.pimoroni.com/article/getting-started-with-badger-2040)
+-   [Badger 2040 function reference](https://github.com/pimoroni/badger2040/blob/main/docs/reference.md)
+-   [Badger 2040 official repository](https://github.com/pimoroni/badger2040)
+-   [GitHub's Badger 2040 repository](https://github.com/badger2040/badgerbodger)
+-   [Badger 2040 Schedule](https://github.com/creativenucleus/badger-2040-schedule)
---
+++ examples/mname.py
@@ -0,0 +1,78 @@
+import badger2040
+import badger_os
+
+MARQUEE_START_X_POSN = -210
+MARQUEE_ORIG_X_POSN = 44
+is_warning_displayed = False
+
+badger = badger2040.Badger2040()
+
+def write_original_text():
+    badger.set_update_speed(badger2040.UPDATE_MEDIUM)
+
+    badger.set_pen(15)
+    badger.clear()
+
+    badger.set_pen(0)
+    badger.set_font("bitmap8")
+    badger.text("Harsh", 35, 5, scale=9)
+    badger.text("Kapadia", MARQUEE_ORIG_X_POSN, 78, scale=6)
+    badger.update()
+
+# Move text from left to right in a loop
+def marquee_text():
+    global is_warning_displayed
+    i = MARQUEE_ORIG_X_POSN
+
+    if is_warning_displayed:
+        is_warning_displayed = False
+        write_original_text()
+
+    badger.set_update_speed(badger2040.UPDATE_TURBO)
+
+    while True:
+        i = i + 10
+
+        badger.set_pen(15)
+        badger.clear()
+
+        badger.set_pen(0)
+        badger.set_font("bitmap8")
+        badger.text("Kapadia", i, 78, scale=6)
+        badger.partial_update(0, 72, badger2040.WIDTH, badger2040.HEIGHT - 72)
+
+        if i >= badger2040.WIDTH:
+            i = MARQUEE_START_X_POSN
+        elif badger.pressed(badger2040.BUTTON_C):
+            write_original_text()
+            break
+        elif badger.pressed(badger2040.BUTTON_B):
+            is_warning_displayed = True
+            badger.set_update_speed(badger2040.UPDATE_MEDIUM)
+            badger_os.warning(badger, "Press 'A' to start looping text from left to right. Press 'C' to stop.")
+            break
+
+def main():
+    global is_warning_displayed
+
+    write_original_text()
+
+    while True:
+        # Sometimes a button press or hold will keep the system
+        # powered *through* HALT, so latch the power back on.
+        badger.keepalive()
+
+        if badger.pressed(badger2040.BUTTON_A):
+            marquee_text()
+        elif badger.pressed(badger2040.BUTTON_B):
+            if not is_warning_displayed:
+                is_warning_displayed = True
+                badger_os.warning(badger, "Press 'A' to start looping text from left to right. Press 'C' to stop.")
+            else:
+                is_warning_displayed = False
+                write_original_text()
+
+        # If on battery, halt the Badger to save power, it will wake up if any of the front buttons are pressed
+        badger.halt()
+
+main()
